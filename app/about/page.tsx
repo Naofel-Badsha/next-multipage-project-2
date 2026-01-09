3560009dff1735f0f32d5e01e602b8e2b463818d
sayhan <samiulsayhan@gmail.com>
2026-01-09 12:32:24 +0600

    added leadership

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import AboutBanner from './AboutSection/AboutBanner';
 import OurStory from './AboutSection/OurStory';
+import LeadershipTeam from './AboutSection/LeadershipTeam';
 
 const AbouUsPage = () => {
     return (
         <div>
             <AboutBanner />
             <OurStory />
+            <LeadershipTeam />
         </div>
     );
 };
